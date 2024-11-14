83a3003a01e0a8f318a9b9d52226767c360c5dd3
IceEmperor4944 <macaroni49344@gmail.com>
2024-11-13 18:39:08 -0700

    Added Unity

---
+++ Assets/important/Scripts/MaterialController.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    void Start()
+    {
+        print("hello, world!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
